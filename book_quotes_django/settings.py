62aeba6fe54c9d451eb19c714ccbfdf860890bbc
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-06-22 17:49:37 +0100

    Publish quotes

--- book_quotes_django/settings.py
+++ book_quotes_django/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'quotes.apps.QuotesConfig',
+    'django_htmx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 from .models import Quote
+from .forms import QuoteForm
 
 
 def index(request):
     quotes = Quote.objects.order_by('-created_at')
-    return render(request, 'quotes/index.html', {'quotes': quotes})
+    form = QuoteForm()
+    return render(request, 'quotes/index.html', {'quotes': quotes, 'form': form})
+
+
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+
+        if form.is_valid():
+            quote = form.save()
+            return render(request, 'quotes/_quote.html', {'quote': quote})
